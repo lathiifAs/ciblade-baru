a4d2786467d1f94e77bc92e5bd00209e2fcbc363
lathiifAs <ajisanthoshol@gmail.com>
2019-11-24 18:10:54 +0700

    dynamis navigation

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -9,9 +9,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         protected $blade;
         protected $tmp_var=[];
         protected $tmp_data;
+        protected $tmp_navbar;
+        // init portal variable
+        protected $portal_id;
+        protected $com_portal;
+        protected $com_user;
+        protected $nav_id = 0;
+        protected $parent_id = 0;
+        protected $parent_selected = 0;
+        protected $role_tp = array();
     
         public function __construct(){
             parent::__construct();
+            $this->load->model('sistem/M_site');
             $this->blade = new PhpBlade($this->views, $this->cache);
         }
 
@@ -34,6 +44,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 $u_login = array('user_login' => $this->session->userdata('com_user'));
             }
 
+            // display current page
+            self::_display_current_page();
+            //display authorize
+            self::_check_authority();
+            // display sidebar navigation
+            self::_display_sidebar_navigation();
+
             $content = ['content' => $content];
             //get semget url
             $uri_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -42,9 +59,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 $segment = array(
                     'seg_menu' => $uri_segments[3]
                 );
-                $params_parsing = array_merge($content, $this->tmp_var,$data, $this->tmp_data, $segment,$u_login);
+                $params_parsing = array_merge($content, $this->tmp_var,$data, $this->tmp_data, $this->tmp_navbar, $segment,$u_login);
             }else{
-                $params_parsing = array_merge($content, $this->tmp_var,$data, $this->tmp_data, $u_login);
+                $params_parsing = array_merge($content, $this->tmp_var,$data, $this->tmp_data, $this->tmp_navbar, $u_login);
             }
             $all = array('all_data' => $params_parsing); 
             //change name of array to js     
@@ -58,6 +75,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 unset($all['all_data'][1]); 
             }  
             $update_params = $all['all_data'];
+            // echo "<pre>";print_r($update_params);exit;
             echo $this->blade->view()->make('template/template',  $update_params);
         }
 
@@ -151,6 +169,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         {
             return $this->tmp_data = $data;
         }
+        
+        public function parsing_navbar($navbar)
+        {
+            return $this->tmp_navbar = $navbar;
+        }
 
         //get data login
         public function get_login($key=[])
@@ -172,5 +195,158 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
         }
 
+        /* --
 
+        System Base
+        
+        */
+
+        private function _display_current_page() {
+            // get current page (segment 1 : folder, segment 2 : sub folder, segment 3 : controller)
+            $url_menu = $this->uri->segment(1) . '/' . $this->uri->segment(2);
+            if (is_dir(APPPATH . 'controllers' . '/' . $this->uri->segment(1) . '/' . $this->uri->segment(2))) {
+                $url_menu .= '/' . $this->uri->segment(3);
+            }
+            $url_menu = trim($url_menu, '/');
+            $url_menu = (empty($url_menu)) ? $this->com_user('default_page') : $url_menu;
+            
+            $result = $this->M_site->get_current_page($url_menu);
+            
+            // print_r($result);die();
+            if (!empty($result)) {
+                $this->parsing_navbar([
+                    'page' => $result
+                ]);
+                $this->nav_id = $result['nav_id'];
+                $this->parent_id = $result['parent_id'];
+            
+            }
+        }
+
+        // authority
+        protected function _check_authority() {
+            // default rule tp
+            $this->role_tp = array("C" => "0", "R" => "0", "U" => "0", "D" => "0");
+            // check
+            if (!empty($this->get_login())) {
+                // user authority
+                $params = array($this->get_login('user_id'), $this->nav_id);
+                $role_tp = $this->M_site->get_user_authority_by_nav($params);
+                // get rule tp
+                $i = 0;
+                foreach ($this->role_tp as $rule => $val) {
+                    $N = substr($role_tp, $i, 1);
+                    $this->role_tp[$rule] = $N;
+                    $i++;
+                }
+            } else {
+                // tidak memiliki authority
+                echo "Anda tidak memiliki authority !";exit;
+            }
+        }
+
+        // sidebar navigation
+        protected function _display_sidebar_navigation() {
+            $html = "";
+            // get data
+            $params = array($this->get_login('user_id'), 0);
+            $rs_id = $this->M_site->get_navigation_user_by_parent($params);
+            if (!empty($rs_id)) {
+                foreach ($rs_id as $rec) {
+                    // check selected
+                    $parent_selected = self::_get_parent_group($this->parent_id, $this->parent_selected);
+                    if ($parent_selected == 0) {
+                        $parent_selected = $this->nav_id;
+                    }
+                    // get child navigation
+                    $child = $this->_get_child_navigation($rec['nav_id']);
+                    if (!empty($child)) {
+                        $url_parent = 'javascript:void(0)';
+                        $sub_toggle = 'class="sidebar-sub-toggle"';
+                        $data_toggle = '<span class="sidebar-collapse-icon ti-angle-down"></span>';
+                    } else {
+                        $url_parent = site_url($rec['nav_url']);
+                        $data_toggle = '';
+                        $sub_toggle = '';
+                    }
+                    // selected
+                    $selected = ($rec['nav_id'] == $parent_selected) ? 'active' : '';
+
+                    //parse 
+                    $html .='
+                    <li class="'.$selected.'">
+                        <a  href="'.$url_parent.'" '.$sub_toggle.'><i class="'.$rec['nav_icon'].'"></i>'.$rec['nav_title'] . $data_toggle .'</a>
+                            '.$child.'
+                    </li>';
+                    
+                    // // parse
+                    // $html .= '
+                    // <li class="slide">
+                    //   <a class="side-menu__item" data-toggle="'.$data_toggle.'" href="$url_parent" title="'.$rec['nav_desc'].'">
+                    //     <i class="side-menu__icon '.$rec['nav_icon'].'"></i>
+                    //     <span class="side-menu__label">'.$rec['nav_title'].'</span>
+                    //     '.$parent_class_caret.'
+                    //   </a>
+                    //   '.$child.'
+                    // </li>';
+                }
+            }
+            // output
+            $this->parsing_navbar([
+                'list_sidebar_nav' => $html
+            ]);
+        }
+        
+        // utility to get parent selected
+        protected function _get_parent_group($int_nav, $int_limit) {
+            $selected_parent = 0;
+            $result = $this->M_site->get_menu_by_id($int_nav);
+            if (!empty($result)) {
+                if ($result['parent_id'] == $int_limit) {
+                    $selected_parent = $result['nav_id'];
+                } else {
+                    return self::_get_parent_group($result['parent_id'], $int_limit);
+                }
+            } else {
+                $selected_parent = $result['nav_id'];
+            }
+            return $selected_parent;
+        }
+
+            // get child
+        protected function _get_child_navigation($parent_id) {
+            $html = '';
+            // get parent selected
+            $parent_selected = self::_get_parent_group($this->parent_id, $parent_id);
+            if ($parent_selected == 0) {
+                $parent_selected = $this->nav_id;
+            }
+            $params = array($this->get_login('user_id'), $parent_id);
+            $rs_id = $this->M_site->get_navigation_user_by_parent($params);
+            if (!empty($rs_id)) {
+                $html .= '<ul>';
+                foreach ($rs_id as $rec) {
+                    // get child navigation
+                    $child = $this->_get_child_navigation($rec['nav_id']);
+                    if (!empty($child)) {
+                        $url_parent = 'javascript:void(0)';
+                    } else {
+                        $url_parent = site_url($rec['nav_url']);
+                    }
+                    // selected
+                    $selected = ($rec['nav_id'] == $parent_selected) ? 'active' : '';
+
+                    // <li><a href="{{ site_url('welcome') }}">Dashboard 1</a></li>
+                    // parse
+                    $html .= '<li>';
+                    $html .= '<a href="' . $url_parent . '">'. $rec['nav_title'] . '</a>';
+                    $html .= $child;
+                    $html .= '</li>';
+                }
+                $html .= '</ul>';
+            }
+            // return
+            return $html;
+        }
+        
     }
--- application/views/template/sidebar.blade.php
+++ application/views/template/sidebar.blade.php
@@ -3,33 +3,7 @@
         <div class="nano">
             <div class="nano-content">
                 <ul>
-                    <li @if (empty($seg_menu)) class="active" @endif>
-                        <a class="sidebar-sub-toggle"><i class="ti-home"></i> Dashboard <span class="sidebar-collapse-icon ti-angle-down"></span></a>
-                        <ul>
-                            <li><a href="{{ site_url('welcome') }}">Dashboard 1</a></li>
-                             
-                        </ul>
-                    </li>
-                    <li><a target="_blank" href="{{ base_url('assets/template_master/nixon-free/index.html') }}"><i class="ti-file"></i> Documentation</a></li>
-                    <li @if (!empty($seg_menu) and $seg_menu == 'master') class="active" @endif>
-                            <a class="sidebar-sub-toggle"><i class="ti-harddrives"></i> Master Data <span class="sidebar-collapse-icon ti-angle-down"></span></a>
-                            <ul>
-                                <li><a href="{{ site_url('master/user') }}">User</a></li>
-                            </ul>
-                    </li>
-                    @if($user_login['role_nm'] == 'developer')
-                        <li @if (!empty($seg_menu) and $seg_menu == 'role') class="active" @endif>
-                                <a class="sidebar-sub-toggle"><i class="ti-settings"></i> Sistem <span class="sidebar-collapse-icon ti-angle-down"></span></a>
-                                <ul>
-                                    <li><a href="{{ site_url('sistem/navigation') }}"> Menu Navigasi</a></li>
-                                    <li><a href="{{ site_url('sistem/group') }}">Group</a></li>
-                                    <li><a href="{{ site_url('sistem/role') }}">Role</a></li>
-                                    <li><a href="{{ site_url('sistem/permission') }}">Hak Akses</a></li>
-                                </ul>
-                        </li>
-                    @endif
-                    <li><a href="{{ site_url('client/beranda') }}" style='color:#00838F'> <i class="ti-home"></i> Beranda</a></li>
-                    <li><a href="{{ site_url('sistem/login/logout') }}"><i class="ti-close"></i> Logout</a></li>
+                    {!! $list_sidebar_nav !!}
                 </ul>
             </div>
         </div>
